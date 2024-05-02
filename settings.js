72879e1374a56d1a7efc76732d1579851e2a7815
dilrukshika1 <168704841+dilrukshika1@users.noreply.github.com>
2024-05-02 22:32:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,8 +16,8 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94773736172",
-  SUDO_NUMBERS : "94773736172",
+  OWNER_NUMBER: "94716787803",
+  SUDO_NUMBERS : "94716787803",
   OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
   BOT_NAME : "ＺＵＳＹＣＯ",
   ZUSYCO_FILE : "ZUSYCO-QR-9.nima",

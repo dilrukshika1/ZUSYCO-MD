5cd7b564d346db2bf13a0adb6fa3019b2b684cae
dilrukshika1 <168704841+dilrukshika1@users.noreply.github.com>
2024-05-03 23:54:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,7 +20,7 @@ module.exports = {
   SUDO_NUMBERS : "94773736172",
   OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
   BOT_NAME : "ＺＵＳＹＣＯ",
-  ZUSYCO_FILE : "ZUSYCO-QR.nima",
+  ZUSYCO_FILE : "ZUSYCO-QR-4.nima",
   LOGOS: {
     ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
     WELCOME: 'https://i.ibb.co/PzhpBV7/WELCOME-IMAGE-LOWQ.jpg',
@@ -55,7 +55,7 @@ module.exports = {
   BOT_OFFLINE : false,
   INBOX_BLOCK : false,
   AUTO_REACT : true,
-  OWNER_REACT : '❤️',
+  OWNER_REACT : '👍',
   AUTO_BGM : false,
   BGM : {
     "chaio" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-caio.mp3",
